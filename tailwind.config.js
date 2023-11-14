767bcd3c232c576f4e3a890c8bd36a88eb839e7b
llNovall <25434267+llNovall@users.noreply.github.com>
2023-11-14 21:12:32 +0100

    Added tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{html,ts,scss}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
