f56e6a1a974aaeebe7d1d489cf35796bf7cb05a0
llNovall <25434267+llNovall@users.noreply.github.com>
2023-11-20 14:36:54 +0100

    Footer dynamically changes position based on the content in viewport

--- src/app/core/layouts/footer/footer.component.ts
+++ src/app/core/layouts/footer/footer.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'core-layouts-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   copyrightDate: Date;
+  observer: ResizeObserver | undefined;
+  isPositionFixed: boolean = false;
+  routerNavigationEnd$ : Subscription;
 
-  constructor() {
+  constructor(private router: Router) {
     this.copyrightDate = new Date();
+
+    this.routerNavigationEnd$ = router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.checkPositionFixedStatus();
+      });
+  }
+
+  ngOnInit(): void {
+    this.observer = new ResizeObserver((e) => {
+      this.checkPositionFixedStatus();
+    });
+
+    this.observer.observe(document.body);
+  }
+
+  ngOnDestroy(): void {
+    if (this.observer) this.observer.unobserve(document.body);
+    if(this.routerNavigationEnd$) this.routerNavigationEnd$.unsubscribe();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkPositionFixedStatus();
+  }
+
+  checkPositionFixedStatus(): void {
+    this.isPositionFixed =
+      document.body.scrollHeight + 54 < document.documentElement.clientHeight;
   }
 }
