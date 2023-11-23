e91fafc68141e46a6cb0b85b529033ed477ee338
llNovall <25434267+llNovall@users.noreply.github.com>
2023-11-23 03:00:21 +0100

    Created context for identity

---
+++ Backend/EFAccess/Context/AppEFContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.Account;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFAccess.Context
+{
+    public class AppEFContext : IdentityDbContext<AppUser>
+    {
+        public AppEFContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
