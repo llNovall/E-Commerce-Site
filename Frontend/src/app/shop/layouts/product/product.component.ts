9cb8e26853472c3ce543c4e7d3f59c78aeda7dc0
llNovall <25434267+llNovall@users.noreply.github.com>
2023-11-25 01:33:12 +0100

    Added layout to display product

---
+++ Frontend/src/app/shop/layouts/product/product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shop-layouts-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent {
+
+}
