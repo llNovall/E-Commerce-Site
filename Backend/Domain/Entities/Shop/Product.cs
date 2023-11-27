db61b88c399bb9b560a67b249d0adb0136b36b28
llNovall <25434267+llNovall@users.noreply.github.com>
2023-11-27 23:40:21 +0100

    Changed price variable from decimal to double

--- Backend/Domain/Entities/Shop/Product.cs
+++ Backend/Domain/Entities/Shop/Product.cs
@@ -4,7 +4,9 @@
     {
         public string? ProductName { get; set; }
         public string? ProductDescription { get; set; }
+        public string? ProductType { get; set; }
+        public string? ProductImgUrl { get; set; }
         public int Quantity { get; set; }
-        public decimal Price { get; set; }
+        public double Price { get; set; }
     }
 }
