7c3a761f284a3abfa84ce27126048b55bc745c13
llNovall <25434267+llNovall@users.noreply.github.com>
2023-11-27 23:40:03 +0100

    Fixed mongodb object not generating id upon insert

--- Backend/Domain/Entities/Shop/BaseEntity.cs
+++ Backend/Domain/Entities/Shop/BaseEntity.cs
@@ -1,7 +1,12 @@
-﻿namespace Domain.Entities.Shop
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Domain.Entities.Shop
 {
     public class BaseEntity
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
     }
 }
